545068a5740fc6e97516002802c9bb1eb29a4f82
mario9502 <njr9502@gmail.com>
2024-07-23 16:19:21 +0300

    Initial commit

---
+++ src/main/java/bg/softuni/regalcinema/RegalCinemaApplication.java
@@ -0,0 +1,13 @@
+package bg.softuni.regalcinema;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RegalCinemaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RegalCinemaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/bg/softuni/regalcinema/RegalCinemaApplicationTests.java
@@ -0,0 +1,13 @@
+package bg.softuni.regalcinema;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RegalCinemaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
